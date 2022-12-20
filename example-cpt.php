11c4870eaebcc3ad3c04330afe8374f92d81455d
Carlos Bonilla <carlosbonilla@isboli.com>
2022-12-19 21:14:35 -0600

    Add the rest of the labels and fix indentation

--- example-cpt.php
+++ example-cpt.php
@@ -6,41 +6,52 @@ Description: A plugin that registers a Custom Post Type and exposes it through t
 Version: 1.0
 Author: Carlos Bonilla
 Author URI: https://www.isboli.com/
+Text Domain: cb-example-cpt
 */
 
-if ( ! class_exists( 'cb_example_cpt' ) ) 
-{
+
+if (!class_exists('cb_example_cpt')) {
   class cb_example_cpt
   {
+
     function __construct()
     {
-      add_action( 'init', array( $this, 'register_example_post_type' ) );
+      add_action('init', array($this, 'register_example_post_type'));
     }
 
+    /**
+     * We're creating a new post type called "Example CPT" 
+     * and registering it with WordPress
+     */
     public function register_example_post_type()
     {
+      // Arguments need it for register the Custom Post Type
       $example_post_type_args = array(
-                                  'label' => 'Example CPT',
-                                  'labels' => array(
-                                    'name' => __( 'Example CPT' ),
-                                    'singular_name' => __( 'Example CPT' ),
-                                    'add_new'=>'Add Example CPT',
-                                    'add_new_item'=>'Add New Example CPT',
-                                    'edit_item'=>'Edit Example CPT',
-                                    'new_item'=>'New Example CPT',
-                                    'view_item'=>'View Example CPT',
-                                    'search_items'=>'Search Example CPT',
-                                  ),
-                                  'public' => true,
-                                  'has_archive' => false,
-                                  'rewrite' => array(
-                                      'slug' => 'example-cpt'
-                                  ),
-                                );
+        'label' => 'Example CPT',
+        'labels' => array(
+          'name'               => __('Example CPTs', 'cb-example-cpt'),
+          'singular_name'      => __('Example CPT', 'cb-example-cpt'),
+          'add_new'            => __('Add New', 'cb-example-cpt'),
+          'add_new_item'       => __('Add New Example CPT', 'cb-example-cpt'),
+          'edit'               => __('Edit', 'cb-example-cpt'),
+          'edit_item'          => __('Edit Example CPT', 'cb-example-cpt'),
+          'new_item'           => __('New Example CPT', 'cb-example-cpt'),
+          'view'               => __('View Example CPT', 'cb-example-cpt'),
+          'view_item'          => __('View Example CPT', 'cb-example-cpt'),
+          'search_items'       => __('Search Example CPT', 'cb-example-cpt'),
+          'not_found'          => __('No Example CPTs found', 'cb-example-cpt'),
+          'not_found_in_trash' => __('No Example CPTs found in Trash', 'cb-example-cpt'),
+        ),
+        'public' => true,
+        'has_archive' => true,
+        'rewrite' => array(
+          'slug' => 'example-cpt'
+        )
+      );
 
-      register_post_type( 'example-cpt', $example_post_type_args );
+      register_post_type('example-cpt', $example_post_type_args);
     }
   }
 
-  new cb_example_cpt();
+  $example_custom_post_type = new cb_example_cpt();
 }
