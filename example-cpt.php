eeef50d9b160085d22b8103697bf23167a2d6882
Carlos Bonilla <carlosbonilla@isboli.com>
2022-12-19 23:38:43 -0600

    Add a bit of security

--- example-cpt.php
+++ example-cpt.php
@@ -9,111 +9,112 @@ Author URI: https://www.isboli.com/
 Text Domain: cb-example-cpt
 */
 
+defined('ABSPATH') or die('Nope, Sorry, Not here.');
 
-if (!class_exists('cb_example_cpt')) {
-  class cb_example_cpt
+class CB_Example_CPT
+{
+
+  function __construct()
   {
-
-    function __construct()
-    {
-      register_activation_hook(__FILE__, array($this, 'cb_create_role'));
-      register_deactivation_hook(__FILE__, array($this, 'cb_remove_role'));
-      add_action('init', array($this, 'cb_register_example_post_type'));
-      add_action('admin_init', array($this, 'cb_add_role_capabilities'), 999);
-    }
-
-    /**
-     * Create a new post type called "Example CPT" 
-     * and register it with WordPress
-     */
-    public function cb_register_example_post_type()
-    {
-      // Arguments need it for register the Custom Post Type
-      $example_post_type_args = array(
-        'label' => 'Example CPT',
-        'labels' => array(
-          'name'               => __('Example CPTs', 'cb-example-cpt'),
-          'singular_name'      => __('Example CPT', 'cb-example-cpt'),
-          'add_new'            => __('Add New', 'cb-example-cpt'),
-          'add_new_item'       => __('Add New Example CPT', 'cb-example-cpt'),
-          'edit'               => __('Edit', 'cb-example-cpt'),
-          'edit_item'          => __('Edit Example CPT', 'cb-example-cpt'),
-          'new_item'           => __('New Example CPT', 'cb-example-cpt'),
-          'view'               => __('View Example CPT', 'cb-example-cpt'),
-          'view_item'          => __('View Example CPT', 'cb-example-cpt'),
-          'search_items'       => __('Search Example CPT', 'cb-example-cpt'),
-          'not_found'          => __('No Example CPTs found', 'cb-example-cpt'),
-          'not_found_in_trash' => __('No Example CPTs found in Trash', 'cb-example-cpt'),
-        ),
-        'public' => true,
-        'has_archive' => true,
-        'rewrite' => array(
-          'slug' => 'example-cpt'
-        )
-      );
-
-      register_post_type('example-cpt', $example_post_type_args);
-    }
-
-    /**
-     * Creates new role called "Example CPT" with the ability to create, 
-     * edit, and delete Example Custom Posts
-     */
-    public function cb_create_role()
-    {
-      $capabilities = array(
-        'edit_post' => true,
-        'read_post' => true,
-        'delete_post' => true,
-        'edit_posts' => true,
-        'edit_others_posts' => true,
-        'publish_posts' => true,
-        'read_private_posts' => true,
-        'read' => true,
-        'delete_posts' => true,
-        'delete_private_posts' => true,
-        'delete_published_posts' => true,
-        'delete_others_posts' => true,
-        'edit_private_posts' => true,
-        'edit_published_posts' => true,
-        'create_posts' => true,
-      );
-
-      add_role('example_cpt_role', 'Example CPT', $capabilities);
-    }
-
-
-    /**
-     * Removes the role 'example_cpt_role'
-     */
-    public function cb_remove_role()
-    {
-      remove_role('example_cpt_role');
-    }
-
-    /**
-     * Adds the capabilities to the role
-     */
-    public function cb_add_role_capabilities()
-    {
-      $roles = array('example_cpt_role', 'administrator');
-
-      foreach ($roles as $each_role) {
-        $role = get_role($each_role);
-        $role->add_cap('read');
-        $role->add_cap('read_example_cpts');
-        $role->add_cap('read_private_example_cpts');
-        $role->add_cap('edit_example_cpts');
-        $role->add_cap('edit_example_cpts');
-        $role->add_cap('edit_others_example_cpts');
-        $role->add_cap('edit_published_example_cpts');
-        $role->add_cap('publish_example_cpts');
-        $role->add_cap('delete_others_example_cpts');
-        $role->add_cap('delete_private_example_cpts');
-        $role->add_cap('delete_published_example_cpts');
-      }
-    }
+    register_activation_hook(__FILE__, array($this, 'cb_create_role'));
+    register_deactivation_hook(__FILE__, array($this, 'cb_remove_role'));
+    add_action('init', array($this, 'cb_register_example_post_type'));
+    add_action('admin_init', array($this, 'cb_add_role_capabilities'), 999);
   }
 
+  /**
+   * Create a new post type called "Example CPT" 
+   * and register it with WordPress
+   */
+  public function cb_register_example_post_type()
+  {
+    // Arguments need it for register the Custom Post Type
+    $example_post_type_args = array(
+      'label' => 'Example CPT',
+      'labels' => array(
+        'name'               => __('Example CPTs', 'cb-example-cpt'),
+        'singular_name'      => __('Example CPT', 'cb-example-cpt'),
+        'add_new'            => __('Add New', 'cb-example-cpt'),
+        'add_new_item'       => __('Add New Example CPT', 'cb-example-cpt'),
+        'edit'               => __('Edit', 'cb-example-cpt'),
+        'edit_item'          => __('Edit Example CPT', 'cb-example-cpt'),
+        'new_item'           => __('New Example CPT', 'cb-example-cpt'),
+        'view'               => __('View Example CPT', 'cb-example-cpt'),
+        'view_item'          => __('View Example CPT', 'cb-example-cpt'),
+        'search_items'       => __('Search Example CPT', 'cb-example-cpt'),
+        'not_found'          => __('No Example CPTs found', 'cb-example-cpt'),
+        'not_found_in_trash' => __('No Example CPTs found in Trash', 'cb-example-cpt'),
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'rewrite' => array(
+        'slug' => 'example-cpt'
+      )
+    );
+
+    register_post_type('example-cpt', $example_post_type_args);
+  }
+
+  /**
+   * Creates new role called "Example CPT" with the ability to create, 
+   * edit, and delete Example Custom Posts
+   */
+  public function cb_create_role()
+  {
+    $capabilities = array(
+      'edit_post' => true,
+      'read_post' => true,
+      'delete_post' => true,
+      'edit_posts' => true,
+      'edit_others_posts' => true,
+      'publish_posts' => true,
+      'read_private_posts' => true,
+      'read' => true,
+      'delete_posts' => true,
+      'delete_private_posts' => true,
+      'delete_published_posts' => true,
+      'delete_others_posts' => true,
+      'edit_private_posts' => true,
+      'edit_published_posts' => true,
+      'create_posts' => true,
+    );
+
+    add_role('example_cpt_role', 'Example CPT', $capabilities);
+  }
+
+
+  /**
+   * Removes the role 'example_cpt_role'
+   */
+  public function cb_remove_role()
+  {
+    remove_role('example_cpt_role');
+  }
+
+  /**
+   * Adds the capabilities to the role
+   */
+  public function cb_add_role_capabilities()
+  {
+    $roles = array('example_cpt_role', 'administrator');
+
+    foreach ($roles as $each_role) {
+      $role = get_role($each_role);
+      $role->add_cap('read');
+      $role->add_cap('read_example_cpts');
+      $role->add_cap('read_private_example_cpts');
+      $role->add_cap('edit_example_cpts');
+      $role->add_cap('edit_example_cpts');
+      $role->add_cap('edit_others_example_cpts');
+      $role->add_cap('edit_published_example_cpts');
+      $role->add_cap('publish_example_cpts');
+      $role->add_cap('delete_others_example_cpts');
+      $role->add_cap('delete_private_example_cpts');
+      $role->add_cap('delete_published_example_cpts');
+    }
+  }
+}
+
+if (class_exists('cb_example_cpt')) {
   $example_custom_post_type = new cb_example_cpt();
 }
